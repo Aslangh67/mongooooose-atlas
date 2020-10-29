ca334e5d445a5396d2c4f648c925fb3e6f73139f
Mr. A <ag.perch@gmail.com>
2020-10-29 13:46:02 -0700

    updated mongoose.connect to set it up for atlas db

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { 
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false
+ });
 
 db.User.create({ name: "Ernest Hemingway" })
   .then(dbUser => {
